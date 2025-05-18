582c7818a6c71605e3f022e6f8efd99b99087366
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-05-18 22:13:56 +0300

    task 05 -  try 00

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    The square size
+"""
+
+
+class Square:
+    """
+        Building Square class
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+
+            for x in range(self.__size):
+                text = ""
+                for y in range(self.__size):
+                    text += '#'
+                print(text)
