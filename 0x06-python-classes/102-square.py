822efa209a94bbcde44e4c35f77442eb4f852912
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-05 22:09:52 +0300

    task 102 -  try  01

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """
-    The square size
+    The square size with comparison operators
 """
 
 
 class Square:
     """
-        Building Square class
+        Building Square class with comparison based on area
     """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size  # Using the setter for validation
 
     @property
     def size(self):
@@ -21,12 +17,42 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
+
+    # Comparison methods
+    def __eq__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        if not isinstance(other, Square):
+            return NotImplemented
+        return self.area() >= other.area()
