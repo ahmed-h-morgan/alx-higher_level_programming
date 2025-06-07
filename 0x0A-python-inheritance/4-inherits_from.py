b3ee696870b7da76e925f399a0d37b9191d31102
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-08 01:10:32 +0300

    task 4 - try 00

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+4. Only sub class of
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    if the object is an instance of a class
+      that inherited (directly or indirectly)
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
