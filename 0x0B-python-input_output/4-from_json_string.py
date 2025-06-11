15e2bcb7df04d17e85547dac7bc5befdd68059bc
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-12 00:54:57 +0300

    task 04 - try 00

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+4. From JSON string to Object
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+     function that returns an object
+    """
+    return json.loads(my_str)
