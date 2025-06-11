043b5a371f25abed320000e73a43fe477ae6f368
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-12 01:22:33 +0300

    task 06 - try 00

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+6. Create object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, 'r+') as file:
+        return json.load(file)
