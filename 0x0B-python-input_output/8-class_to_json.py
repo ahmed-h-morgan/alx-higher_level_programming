21fcd36bef10139ffef79f3ade9805925369a23f
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-13 00:51:17 +0300

    task 08 - try 01

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,11 @@
 """
 8. Class to JSON
 """
-import json
+
 
 
 def class_to_json(obj):
     """
     a function that returns the dictionary description
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
