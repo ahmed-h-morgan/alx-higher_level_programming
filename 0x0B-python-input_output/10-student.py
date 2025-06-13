30cf2ea6a0c757a6759d4d9482f5f25eed87cb94
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-13 14:45:48 +0300

    task 10 - try 00

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+9. Student to JSON
+"""
+
+
+class Student:
+    """
+    class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """
+        class Student that defines a student by: (based on 9-student.py)
+        """
+        result_dic = {}
+
+        if not attrs:
+            return self.__dict__
+
+        for attribute in attrs:
+            if not isinstance(attribute, str):
+                return self.__dict__
+
+        for attribute in attrs:
+            for keys, values in self.__dict__.items():
+                if attribute in keys:
+                    result_dic[attribute] = values
+        return result_dic
