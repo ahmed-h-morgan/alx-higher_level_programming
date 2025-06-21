aa46c4cfab58358f090191773586e9e82c882516
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-22 00:10:02 +0300

    task 01 - try 00

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1. Base class
+"""
+
+
+class Base:
+    """
+    create the base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
