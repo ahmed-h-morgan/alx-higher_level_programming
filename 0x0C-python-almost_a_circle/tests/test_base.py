3354e388c9b83e6ff77f080c27563eb61fbb9c09
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-06-23 06:43:36 +0300

    task 02 - try 03 - add new test case - new_id

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -12,3 +12,7 @@ class TestBase(unittest.TestCase):
     def test_create_first_object(self):
         first_object = Base()
         self.assertEqual(Base._Base__nb_objects, 1)
+
+    def test_new_id(self):
+        new_id = Base(25)
+        self.assertEqual(new_id.id, 25)
