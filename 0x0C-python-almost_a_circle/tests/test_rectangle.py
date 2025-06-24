3fee1d922493dc4c93d18feb134f1b22a1add887
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-06-24 08:22:52 +0000

    task 00 - try 05 - remove comments from the rectangle test method

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -10,12 +10,10 @@ class TestRectangle(unittest.TestCase):
     Test Rectangle class
     """
     def test_only_width(self):
-        """ only width argument """
         with self.assertRaises(TypeError):
             Rectangle(5)
 
     def test_positive_width_and_height(self):
-        """ only positive width and height """
         Rectangle(10,4)
 
     def test_negative_width(self):
