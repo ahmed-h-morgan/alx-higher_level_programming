9f9082246991a44ef34716b85932cb98dfeb900e
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-06-24 11:28:09 +0000

    task 06 - try 00 - __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,7 @@ class Rectangle(Base):
             for w in range(self.width):
                 row += "#"
             print(row)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
+            {self.width}/{self.height}"
