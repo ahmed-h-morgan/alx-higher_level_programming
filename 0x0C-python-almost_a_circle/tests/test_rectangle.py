a288696062e0850f8b3f0f339ee4cba0295a625c
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-06-24 09:21:38 +0000

    task 00 - try 07 - add new tests to rectangle test module

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -13,9 +13,43 @@ class TestRectangle(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 5)
 
     def test_positive_width_and_height(self):
-        rect = Rectangle(7,14)
-        self.assertEqual(rect.width,7)
+        rect = Rectangle(7, 14)
+        self.assertEqual(rect.width, 7)
         self.assertEqual(rect.height, 14)
 
     def test_negative_width(self):
-        pass
+        self.assertRaisesRegex(ValueError, "width must be > 0", Rectangle, -3, 6)
+
+    def test_negative_height(self):
+        self.assertRaisesRegex(ValueError, "height must be > 0", Rectangle, 7, -5)
+
+    def test_zero_width(self):
+        self.assertRaisesRegex(ValueError, "width must be > 0", Rectangle, 0, 10)
+
+    def test_zero_height(self):
+        self.assertRaisesRegex(ValueError, "height must be > 0", Rectangle, 15, 0)
+
+    def test_negative_x(self):
+        self.assertRaisesRegex(ValueError, "x must be >= 0", Rectangle, 2, 6, -4)
+
+    def test_negative_y(self):
+        self.assertRaisesRegex(ValueError, "y must be >= 0", Rectangle, 2, 6, 4, -7)
+
+    def test_string_width(self):
+        self.assertRaisesRegex(TypeError, "width must be an integer" Rectangle, "3", 6)
+
+    def test_string_height(self):
+        self.assertRaisesRegex(TypeError, "height must be an integer", Rectangle, 12, "16")
+
+    def test_string_x(self):
+        self.assertRaisesRegex(TypeError, "x must be an integer", Rectangle, 12, 16, "3")
+
+    def test_string_y(self):
+        self.assertRaisesRegex(TypeError, "y must be an integer", Rectangle, 12, 16, 3, "5")
+
+    def test_all_positive(self):
+        rect = Rectangle(7, 14, 3, 9)
+        self.assertEqual(rect.width, 7)
+        self.assertEqual(rect.height, 14)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 9)
