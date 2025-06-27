204cc853df6351b1bc105821ea1bfed0af0232e7
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-06-27 13:54:44 +0000

    task 11 - try 00

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,16 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        
+
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        self.size = value
