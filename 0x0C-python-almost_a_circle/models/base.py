6b24b9fbc1893a0040d1ad82d9d01f87098f79f0
Ahmed Hussein Morgan <39451996+ahmed-h-morgan@users.noreply.github.com>
2025-06-28 14:41:03 +0000

    task 17 - try 01 - add from_json_string() method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return []
 
         list_rep = json.loads(json_string)
         return list_rep
